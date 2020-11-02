ed721295db845bfbc5220e55028058fcddd6a0a7
Ronan <galv.ronan@gmail.com>
2020-11-02 14:26:29 -0500

    debug symbols and character length values

--- Develop/script.js
+++ Develop/script.js
@@ -22,9 +22,14 @@ var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 var generatePassword = function() {
   var confirmLength = (prompt("Choose the length of your password. Pick a number from 8-128."))
 
-    if (confirmLength <= 7 || confirmLength >= 129) {
+    while (confirmLength <= 7 || confirmLength >= 129) {
       alert("The password length must be between 8-128 characters. Pick a valid number.");
       var confirmLength = (prompt("Choose the length of your password. Pick a number from 8-128."));
+    }
+
+    if (!(/[0-9]/.test(confirmLength))) {
+      alert("Invalid input, please try again.")
+      generatePassword()
     }
 
     // Alert the user how many characters the password will have
@@ -57,7 +62,7 @@ var generatePassword = function() {
     }
 
     if (confirmSymbols) {
-      paswordCharacters = passwordCharacters.concat(symbols)
+      passwordCharacters = passwordCharacters.concat(symbols)
     }
 
     if (confirmNumeric) {
@@ -86,6 +91,3 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
