e3130adbcaded237f83aed35ef1013ee108fc0c0
Ronan <galv.ronan@gmail.com>
2020-11-02 13:10:28 -0500

    criteria questions

--- Develop/script.js
+++ Develop/script.js
@@ -16,73 +16,34 @@ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+// Prompt to confirm the length of the password
+var generatePassword = function() {
+  var confirmLength = (prompt("Choose the length of your password. Pick a number from 8-128."))
 
+    if (confirmLength <= 7 || confirmLength >= 129) {
+      alert("The password length must be between 8-128 characters. Pick a valid number.");
+      var confirmLength = (prompt("Choose the length of your password. Pick a number from 8-128."));
+    }
 
-//length
-var getLength = function () {
-  passLength = prompt("What is the length of your password? Pick a number from 8-128");
-};
+    // Alert the user how many characters the password will have
+    alert("Your password will have " + confirmLength + " characters.");
 
+    // Determine the criteria of the password
+    var confirmUpper = confirm("Click OK to confirm if you would like to include uppercase characters");
+    var confirmLower = confirm("Click OK to confirm if you would like to include lowercase characters");
+    var confirmSymbols = confirm("Click OK to confirm if you would like to include special characters");
+    var confirmNumeric = confirm("Click OK to confirm if you would like to include numeric characters");
+      // Loop if answer is outside of parameters
+      while(confirmUpper === false && confirmLower === false && confirmSymbols === false && confirmNumeric === false) {
+        alert("You must choose at least one parameter");
+        var confirmUpper = confirm("Click OK to confirm if you would like to include uppercase characters");
+        var confirmLower = confirm("Click OK to confirm if you would like to include lowercase characters");
+        var confirmSymbols = confirm("Click OK to confirm if you would like to include special characters");
+        var confirmNumeric = confirm("Click OK to confirm if you would like to include numeric characters");
+      }
 
-// lowerCase
-var lowerCase = function() {
-  confirm("Would you like to include lower case letters?")
-};
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-};
-//console.log (getRandomLower());
-
-
-// upperCase
-var upperCase = function() {
-  confirm("Would you like to include upper case letters?")
-};
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-};
-
-// numeric
-var numeric = function() {
-  confirm("Would you like to include numeric characters: 0-9?")
+      // Assign 
 }
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-};
-
-// symbols
-var symbols = function() {
-  confirm("Would you like to include symbols? (!@#$%^&*(){}=<>/,.)")
-}
-function getRandomSymbol() {
-  const symbols = '!@#$%^&*(){}=<>/,.'
-  return symbols[Math.floor(Math.random() * symbols.length)];
-};
-
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate")
-
-generateBtn.onclick = function() {
-  alert("Select criteria for password.");
-
-  getLength();
-
-  lowerCase();
-
-  upperCase();
-
-  numeric();
-
-  symbols();
-  console.log(passLength);
-
-  writePassword();
-
-};
-
-// USE SWITCH TO get different results?
 
 
 
@@ -91,41 +52,7 @@ generateBtn.onclick = function() {
 
 
 
-/*
 
-//generate random password
-var generate = function() {
-
-  // set password length of at least 8 characters and no more than 128 characters
- // let complexity =
-
-  //possible password values
-  let values = "All numbers/letters/symbols"
-
-  // when user hits the generate button
-  let passwordGenerated = ""
-
-  // create for loop to choose password characters
-  for(var i = 0; i <= complexity; i++)
-    passwordGenerated = passwordGenerated + values.chartAt(Math.floor(Math.random()*Math.floor(complexity -1)))
-
-  // add password to textbox/display area
-  document.getElementById("display")
-};
-
-//GeneratePassword
-generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-  //  var password = generatePassword();
-
-//GeneratePassword Function
-var generatePassword = function(lower, upper, number, symbol, length) {
-  // 1. init pw variable
-  // 2. filter out unchecked types
-  // 3. loop over length call generator function for each type
-  // 4. add final pw to the pw var and return
-
-}
-*/
 
 var generatePassword = function() {
   for (i = 0; i <= passLength.length; i++) {
