564612fc2fbc8414b3cc1fef8cc948924aa717b4
Ronan <galv.ronan@gmail.com>
2020-11-01 23:25:58 -0500

    develop branch

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,122 @@
 // Assignment code here
 
+// Generator functions - http://www.net-comber.com/charset.html
+
+//length
+var getLength = function () {
+  passLength = prompt("What is the length of your password? Pick a number from 8-128");
+};
+
+
+// lowerCase
+var lowerCase = function() {
+  confirm("Would you like to include lower case letters?")
+};
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+//console.log (getRandomLower());
+
+
+// upperCase
+var upperCase = function() {
+  confirm("Would you like to include upper case letters?")
+};
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+// numeric
+var numeric = function() {
+  confirm("Would you like to include numeric characters: 0-9?")
+}
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+
+// symbols
+var symbols = function() {
+  confirm("Would you like to include symbols? (!@#$%^&*(){}=<>/,.)")
+}
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate")
+
+generateBtn.onclick = function() {
+  alert("Select criteria for password.");
+
+  getLength();
+
+  lowerCase();
+
+  upperCase();
+
+  numeric();
+
+  symbols();
+  console.log(passLength);
+
+  writePassword();
+
+};
+
+// USE SWITCH TO get different results?
+
+
+
+
+
+
+
+
+/*
+
+//generate random password
+var generate = function() {
+
+  // set password length of at least 8 characters and no more than 128 characters
+ // let complexity =
+
+  //possible password values
+  let values = "All numbers/letters/symbols"
+
+  // when user hits the generate button
+  let passwordGenerated = ""
+
+  // create for loop to choose password characters
+  for(var i = 0; i <= complexity; i++)
+    passwordGenerated = passwordGenerated + values.chartAt(Math.floor(Math.random()*Math.floor(complexity -1)))
+
+  // add password to textbox/display area
+  document.getElementById("display")
+};
+
+//GeneratePassword
+generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+  //  var password = generatePassword();
+
+//GeneratePassword Function
+var generatePassword = function(lower, upper, number, symbol, length) {
+  // 1. init pw variable
+  // 2. filter out unchecked types
+  // 3. loop over length call generator function for each type
+  // 4. add final pw to the pw var and return
+
+}
+*/
+
+var generatePassword = function() {
+  for (i = 0; i <= passLength.length; i++) {
+  const answer = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*(){}=<>/,.0123456789'
+  return answer[Math.floor(Math.random() * answer.length)]
+}
+}
 
 // Write password to the #password input
 function writePassword() {
